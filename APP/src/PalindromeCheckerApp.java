ae13a7e3ad9b36e7d2bc92664709d5e512f9cd8a
se5242 <se5242@srmist.edu.in>
2026-03-10 10:29:12 +0530

    UC1: Application Entry & Welcome Message

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to the Palindrome Checker Management System");
+
+        // Display application version
+        System.out.println("Version : 1.0");
+
+        // Display initialization message
+        System.out.println("System initialized successfully.");
+        }
+}
